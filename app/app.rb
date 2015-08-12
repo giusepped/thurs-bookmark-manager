b9be27b76e410cc29ee0df6edb56716bf9043065
giuseppe de santis <giusdesan@gmail.com>
2015-08-12 16:01:16 +0100

    finished stage 4, all green

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class App < Sinatra::Base
   enable :sessions
+  register Sinatra::Flash
   set :session_secret, 'super secret'
 
   get '/' do
@@ -35,15 +37,21 @@ class App < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                        password: params[:password],
                        password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -18,8 +18,9 @@ feature 'User sign up' do
   end
 
   scenario 'requires a matching confirmation password' do
-
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Password and confirmation password do not match')
   end
 
   def sign_up(email: 'alice@example.com',
